e19d0c785065aa7a52a9a9c095a5d32a278f7385
holard <fagbolagbenga2015@gmail.com>
2023-05-05 04:21:02 +0100

    deadlock prevention by TDD

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -118,3 +118,54 @@ func TestTransferTx(t *testing.T) {
 }
 
 
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
+	n := 	10
+	amount := int64(10)
+
+	// since the go routine runs in a different routine from the main TestTransferTx 
+	//func we make use of channels to communicate between respective channels creating one for error and another for results
+	errs := make(chan error)
+
+	// 5 concurrent go transaction transferring money from one account to another
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i % 2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func(){
+			ctx := context.Background()
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount: 	   amount,
+			})
+			errs <- err
+		}()
+	}
+
+	// check result
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
