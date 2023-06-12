b91b1d8793fcb46cbc6c08085924aacdc23dc30c
holard <fagbolagbenga2015@gmail.com>
2023-06-12 03:34:39 +0100

    add db docs

--- api/user_test.go
+++ api/user_test.go
@@ -217,9 +217,9 @@ func TestLoginUserAPI(t *testing.T) {
 					GetUser(gomock.Any(), gomock.Eq(user.Username)).
 					Times(1).
 					Return(user, nil)
-				// store.EXPECT().
-				// 	CreateSession(gomock.Any(), gomock.Any()).
-				// 	Times(1)
+				store.EXPECT().
+					CreateSession(gomock.Any(), gomock.Any()).
+					Times(1)
 			},
 			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusOK, recorder.Code)
