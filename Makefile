157968789e8de931c495517262037e8a1633b123
oluwagbenga fagbola <fagbolagbenga2015@gmail.com>
2023-06-12 03:35:46 +0100

    add db docs (#29)

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+DB_URL=postgresql://root:secretpass@localhost:5432/bank?sslmode=disable
 postgres:
 	docker run --name postgres01 --network bank-network -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secretpass -d postgres:15.2-alpine
 createdb:
@@ -5,13 +6,13 @@ createdb:
 dropdb:
 	docker exec -it postgres01 dropdb --username=root --owner=root bank
 migrateup:
-	migrate -path db/migration -database "postgresql://root:secretpass@localhost:5432/bank?sslmode=disable" -verbose up
+	migrate -path db/migration -database "$(DB_URL)" -verbose up
 migrateup1:
-	migrate -path db/migration -database "postgresql://root:secretpass@localhost:5432/bank?sslmode=disable" -verbose up 1	
+	migrate -path db/migration -database "$(DB_URL)" -verbose up 1
 migratedown:
-	migrate -path db/migration -database "postgresql://root:secretpass@localhost:5432/bank?sslmode=disable" -verbose down
+	migrate -path db/migration -database "$(DB_URL)" -verbose down
 migratedown1:
-	migrate -path db/migration -database "postgresql://root:secretpass@localhost:5432/bank?sslmode=disable" -verbose down 1	
+	migrate -path db/migration -database "$(DB_URL)" -verbose down 1
 sqlc:
 	sqlc generate
 test:
@@ -20,5 +21,10 @@ server:
 	go run main.go
 mock:
 	mockgen -package mockdb -destination db/mock/store.go github.com/techschool/simplebank/db/sqlc Store
-    
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server mock migrateup1 migratedown1
+db_docs:
+	dbdocs build doc/db.dbml
+
+db_schema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server mock migrateup1 migratedown1 db_docs db_schema
--- api/user.go
+++ api/user.go
@@ -84,8 +84,8 @@ type loginUserResponse struct {
 	SessionID             uuid.UUID    `json:"session_id"`
 	AccessToken           string       `json:"access_token"`
 	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
-	// RefreshToken          string       `json:"refresh_token"`
-	// RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	RefreshToken          string       `json:"refresh_token"`
+	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
 	User                  userResponse `json:"user"`
 }
 
@@ -122,28 +122,37 @@ func (server *Server) loginUser(ctx *gin.Context) {
 		return
 	}
 
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
  
 
-	// session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
-	// 	ID:           refreshPayload.ID,
-	// 	Username:     user.Username,
-	// 	RefreshToken: refreshToken,
-	// 	UserAgent:    ctx.Request.UserAgent(),
-	// 	ClientIp:     ctx.ClientIP(),
-	// 	IsBlocked:    false,
-	// 	ExpiresAt:    refreshPayload.ExpiredAt,
-	// })
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	// 	return
-	// }
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    ctx.Request.UserAgent(),
+		ClientIp:     ctx.ClientIP(),
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
 	rsp := loginUserResponse{
-		// SessionID:             session.ID,
+		SessionID:             session.ID,
 		AccessToken:           accessToken,
 		AccessTokenExpiresAt:  accessPayload.ExpiredAt,
-		// RefreshToken:          refreshToken,
-		// RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
 		User:                  newUserResponse(user),
 	}
 	ctx.JSON(http.StatusOK, rsp)
